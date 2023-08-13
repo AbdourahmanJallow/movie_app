3f8b38dfffc4e81269881485aa30487193831bbe
abdourahman <aj22018057@utg.edu.gm>
2023-08-13 01:20:25 +0000

    slight changes

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -4,14 +4,13 @@ import { Observable } from 'rxjs';
 import {
   TrendingMovies,
   Genres,
-  Movie,
   NowPlayingOrUpcoming,
 } from '../models/movies.model';
 import { MovieDetails } from 'src/app/models/movieDetails.model';
+import { environment } from 'src/environments/environment.development';
 
 const apiUrl = 'https://api.themoviedb.org/3';
-const apiKey = 'ab36cac2d26370cf7d5fb65859f53a25';
-const authorizationHeader = `Bearer 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`;
+const apiKey = '';
 
 // const headers = new Headers({
 //   Authorization: authorizationHeader,
@@ -42,6 +41,7 @@ export class MoviesService {
       apiUrl + `/movie/${type}?api_key=${apiKey}`
     );
     // console.log(data);
+    console.log(environment);
     return data;
   }
 
@@ -57,20 +57,3 @@ export class MoviesService {
     );
   }
 }
-
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     Accept: 'application/json',
-//     Authorization:
-//       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjM2Y2FjMmQyNjM3MGNmN2Q1ZmI2NTg1OWY1M2EyNSIsInN1YiI6IjY0ZDI2MmVhNmQ0Yzk3MDBjYjdmNjlhOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HCZLB6wv3KSIMSYvWwe1837AYjLX0kW5XmDAT99FWRc',
-//   },
-// };
-
-// fetch(
-//   'https://api.themoviedb.org/3/movie/top_rated?api_key=ab36cac2d26370cf7d5fb65859f53a25',
-//   options
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
