28a2776c309c6453434737745169360fbbb1afa8
AbdourahmanJallow <aj22018057@utg.edu.gm>
2023-09-03 22:41:57 +0000

    show favorite movies in page

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { OnInit, OnDestroy } from '@angular/core';
+import { FavoriteMovies, Movie } from 'src/app/models/movies.model';
+import { FavoritesService } from 'src/app/services/favorites.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit {
+  public savedMovies: Array<Movie> = [];
 
+  constructor(private favoritesService: FavoritesService) {}
+  ngOnInit(): void {
+    this.favoritesService.movies.subscribe(
+      (data) => (this.savedMovies = data.savedMovies)
+    );
+  }
+  
 }
